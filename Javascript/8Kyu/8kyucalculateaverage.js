9f4004fe11867a16c296c155804debd6111ab2ba
Joe McCarthy <joe_mccarthy22@hotmail.co.uk>
2024-10-24 12:53:18 +0100

    Added 8Kyu Solution

---
+++ Javascript/8Kyu/8kyucalculateaverage.js
@@ -0,0 +1,10 @@
+/* Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0. */
+
+
+
+  function findAverage(array) {
+    let sum = array.reduce((acc, c) => acc + c, 0);
+    return sum / array.length
+  }
