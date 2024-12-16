64baa63d393df9d02908469ca8c51c8eaa38ea2c
Joe McCarthy <joe_mccarthy22@hotmail.co.uk>
2024-12-16 12:20:22 +0000

    7kyu codewars down

---
+++ Javascript/7Kyu/7kyuSumOfTwoLowestIntegers.js
@@ -0,0 +1,18 @@
+/* Challenge: Sum of Two Lowest Positive Integers
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or empty arrays will be passed to the function.
+
+Example:
+
+sumTwoSmallestNumbers([19, 5, 42, 2, 77]) // returns 7
+*/
+
+/* P: an array of min 4 positive integers
+R: sum of two smallest integers
+E: as above
+Ps: sort the array
+return array[0] + array[1] */
+
+function sumTwoSmallestNumbers(array){
+    let array2 = array.sort((a, b) => a - b)
+    return array2[0] + array2[1]
+}
