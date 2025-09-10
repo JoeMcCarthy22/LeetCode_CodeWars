e9d6e0d6fba9dbda6bb8b61deb2813cf7cf9d034
Joe McCarthy <joe_mccarthy22@hotmail.co.uk>
2025-09-10 16:02:59 +0100

    6kyu

---
+++ Javascript/6Kyu/6kyuLinkedLists.js
@@ -0,0 +1,31 @@
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function length(head) {
+  let count = 0;
+  let current = head;
+  
+  while (current !== null){
+    count++
+    current = current.next
+  }
+  
+  return count
+  
+}
+
+function count(head, data) {
+  let current = head;
+  let count = 0;
+  
+  while (current !== null){
+    if (current.data == data){
+      count++;
+    }
+     current = current.next
+  }
+  
+  return count
+}
