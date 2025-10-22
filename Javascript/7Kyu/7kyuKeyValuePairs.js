db59af7795b047f75f724c20f4618731428cff30
JoeMcCarthy22 <Joe_McCarthy22@hotmail.co.uk>
2025-10-22 10:53:35 +0100

    7kyu

---
+++ Javascript/7Kyu/7kyuKeyValuePairs.js
@@ -0,0 +1,19 @@
+/*
+P: a hash map of languagees and corresponding score
+R: an array of the languages where the corresponding score is at least 60
+PS: create a result array
+loop through the input results
+if results[score] >= 60, push to result array
+return the result array
+*/
+
+function myLanguages(results) {
+  
+ let languages = Object.keys(results);
+  
+  let passed = languages.filter( lang => results[lang] >= 60);
+  
+  return passed.sort((a, b) => results[b] - results[a]);
+  
+
+}
