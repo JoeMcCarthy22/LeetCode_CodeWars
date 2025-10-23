133ba91cf44d2c061f05d3520ff04dc2ca38bda1
JoeMcCarthy22 <Joe_McCarthy22@hotmail.co.uk>
2025-10-23 17:32:28 +0100

    7kyu

---
+++ Javascript/7Kyu/7kyuDigitsExlposion.js
@@ -0,0 +1,24 @@
+/*
+P: a string made of digits
+R: a string where each digit is repeated * its value
+Ps: split the string into an array
+create a seperate result array
+loop through input
+push each digit x times 
+join and return
+*/
+
+
+
+function explode(s) {
+  
+  let input = s.split('');
+  let output = [];
+  
+  for (let i = 0; i < input.length; i++){
+    output.push(input[i].repeat(input[i]))
+  }
+  
+  return output.join('');
+  
+}
