6f5b11b188a222c46afccaa58c9b6cd9399fe06e
JoeMcCarthy22 <Joe_McCarthy22@hotmail.co.uk>
2025-11-17 15:54:03 +0000

    7kyu

--- Javascript/7Kyu/7kyuAlphabetWar.js
+++ Javascript/7Kyu/7kyuAlphabetWar.js
@@ -1,7 +1,7 @@
 /*
 Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
 
-The left side letters and their power:
+The left sife letters and their power:
 
  w - 4
  p - 3
