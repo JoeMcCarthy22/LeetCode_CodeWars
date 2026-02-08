a4e2274361f0bc381ac7b3c01593aac4fd62e150
JoeMcCarthy22 <Joe_McCarthy22@hotmail.co.uk>
2026-02-08 19:58:13 +0000

    more interview prep!

--- Javascript/6Kyu/6kyuVisaPracticeQs.js
+++ Javascript/6Kyu/6kyuVisaPracticeQs.js
@@ -50,3 +50,69 @@ function getSuccessfulTotal(transactions){
     return successfuls.reduce((acc, tx) => acc + tx.amount, 0)
 }
 
+
+/*
+
+PROBLEM 2:
+
+Practice Problem: Top Spenders
+You have an array of transactions. Each transaction is an object like this:
+
+{
+  user: "Alice",
+  amount: 50,
+  status: "SUCCESS"
+}
+
+
+Write a function getTopSpenders(transactions, threshold) that:
+Filters out only successful transactions.
+Sums the total amount per user.
+Returns an array of user names whose total spending is greater than or equal to the threshold.
+
+Example:
+
+const transactions = [
+  { user: "Alice", amount: 50, status: "SUCCESS" },
+  { user: "Bob", amount: 20, status: "FAILED" },
+  { user: "Alice", amount: 70, status: "SUCCESS" },
+  { user: "Bob", amount: 100, status: "SUCCESS" }
+];
+
+getTopSpenders(transactions, 100); // should return ["Alice", "Bob"]
+
+P: an array of objects, and the threshold value
+R: an Array of Names who spend over the threshold
+Ps:
+-filter out all the non successful transactions
+-create an object to store each spenders total
+-then reduce.() to calculate if the total meets the threshold
+-if so, push to an array and return
+
+*/
+
+function getTopSpenders(transactions, threshold){
+
+  let succs = transactions.filter(tx => tx.status === 'SUCCESS');
+
+  let spendersTotal = {};
+
+  for (let i = 0; i < succs.length; i++){
+    let user = succs[i].user;
+    let amount = succs[i].amount;
+
+    if (!spendersTotal[user]) spendersTotal[user] = 0;
+
+    spendersTotal[user] += amount;
+  }
+
+  let bigSpenders = [];
+
+  for (let key in spendersTotal){
+    if (spendersTotal[key] >= threshold){
+      bigSpenders.push(key)
+    }
+  }
+
+  return bigSpenders
+}
